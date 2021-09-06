733cbffbf3ea4498faeee8709106d23d5a27cb0d
Thomas Maxey <tmaxey201@gmail.com>
2021-09-06 16:56:26 -0400

    created api.js file in routes

--- server.js
+++ server.js
@@ -15,3 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
