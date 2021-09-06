d64f47e9109185cae19bdc0e8ddf8499395998db
Thomas Maxey <tmaxey201@gmail.com>
2021-09-06 16:55:20 -0400

    added server.js code

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const compression = require("compression");
 const PORT = 3000;
 
 const app = express();
+
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
